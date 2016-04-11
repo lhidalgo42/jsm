41af908bc76cb8ba9a5129ccfb4505701c11de0d
Leonardo Hidalgo <lhidalgo@alumnos.uai.cl>
2016-04-11 10:27:30 -0300

    fix user seeder

--- app/database/seeds/UserTableSeeder.php
+++ app/database/seeds/UserTableSeeder.php
@@ -6,15 +6,16 @@ class UserTableSeeder extends Seeder {
 	public function run()
 	{
 		User::create([
-			'name' => 'Leonardo Hidalgo',
-			'email' => 'lhidalgosep@gmail.com',
-			'password' => '1234'
+				'name' => 'Leonardo Hidalgo',
+				'email' => 'lhidalgosep@gmail.com',
+				'password' => '1234'
 		]);
 		User::create([
 				'name' => 'Ivan Salinas',
 				'email' => 'isalinas@systemit.cl',
 				'password' => '1234'
 		]);
+	}
 
 
 }
