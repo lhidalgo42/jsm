9321072502b1d664d86001592c7ae4144ed76e3f
Leonardo Hidalgo <lhidalgo@alumnos.uai.cl>
2016-04-17 23:56:05 -0300

    update login

--- app/controllers/SessionsController.php
+++ app/controllers/SessionsController.php
@@ -6,41 +6,40 @@ use Laracasts\Validation\FormValidationException;
 class SessionsController extends \BaseController {
 
 
-    protected $loginForm;
+	protected $loginForm;
 
-    public function __construct(LoginForm $loginForm)
-    {
-        $this->loginForm = $loginForm;
-    }
-    public function create()
-    {
-        // Verificamos que el usuario no esté autenticado
-        if (Auth::check())
-        {
-            return Redirect::route('home');
-        }
+	public function __construct(LoginForm $loginForm)
+	{
+		$this->loginForm = $loginForm;
+	}
+	public function create()
+	{
+		// Verificamos que el usuario no esté autenticado
+		if (Auth::check())
+		{
+			return Redirect::route('admin');
+		}
 
-        return View::make('sessions.create');
-    }
-    public function store()
-    {
-        $remember = (Input::has('remember')) ? true : false;
+		return View::make('sessions.create');
+	}
+	public function store()
+	{
+		$remember = (Input::has('remember')) ? true : false;
 
-        $formData = Input::only('email', 'password');
+		$formData = Input::only('email', 'password');
 
-        $this->loginForm->validate($formData);
+		$this->loginForm->validate($formData);
 
-
-        if ( ! Auth::attempt($formData, $remember))
-        {
-            return Redirect::back()->withInput()->with('error', 'Su nombre de Usuario o Su clave son Incorrectos.');
-        }
-        return Redirect::route('home')->with('Success', 'Bienvenido $name$ $lastname$ Nuevamente.');
-    }
-    public function destroy()
-    {
-        Auth::logout();
-        return Redirect::to('/login')->with('success', 'Gracias por su Visita.');
-    }
+		if ( ! Auth::attempt($formData, $remember))
+		{
+			return Redirect::back()->withInput()->with('error', 'Su nombre de Usuario o Su clave son Incorrectos.');
+		}
+		return Redirect::route('admin')->with('Success', 'Bienvenido $name$ $lastname$ Nuevamente.');
+	}
+	public function destroy()
+	{
+		Auth::logout();
+		return Redirect::to('/login')->with('success', 'Gracias por su Visita.');
+	}
 
 }
--- app/routes.php
+++ app/routes.php
@@ -11,8 +11,16 @@
 |
 */
 
+Route::get('/login', ['as' => 'login', 'uses' => 'SessionsController@create']);
+Route::get('/logout', ['as' => 'logout', 'uses' => 'SessionsController@destroy']);
+Route::resource('sessions', 'SessionsController', ['only' => ['create', 'store', 'destroy']]);
+
 Route::get('/','PagesController@index');
 Route::get('/sell','PagesController@sell');
 Route::post('/sendmail','MailController@create');
 Route::post('/getcar/{id}','CarController@show');
 Route::post('/create/car','CarController@create');
+
+Route::group(array('before' => 'auth'), function() {
+    Route::get('/admin', ['as' => 'admin', 'uses' => 'UsersController@admin']);
+});
