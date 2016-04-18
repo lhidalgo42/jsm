bc315d77b85b54da7e5fd95902be1a32cc6afc51
Leonardo Hidalgo <lhidalgo@alumnos.uai.cl>
2016-04-17 21:05:57 -0300

    update visual

--- app/views/layout/master.blade.php
+++ app/views/layout/master.blade.php
@@ -235,7 +235,7 @@
 </script>
 <script type="text/javascript">
     $(".rotate").textrotator({
-        animation: "flip", // You can pick the way it animates when rotating through words. Options are dissolve (default), fade, flip, flipUp, flipCube, flipCubeUp and spin.
+        animation: "flipCube", // You can pick the way it animates when rotating through words. Options are dissolve (default), fade, flip, flipUp, flipCube, flipCubeUp and spin.
         separator: ",", // If you don't want commas to be the separator, you can define a new separator (|, &, * etc.) by yourself using this field.
         speed: 3000 // How many milliseconds until the next word show.
     });
--- app/views/section/header/home.blade.php
+++ app/views/section/header/home.blade.php
@@ -3,10 +3,10 @@
     $photos = PhotoFile::lists('name');
     $count = count($photos) -1;
 
-    if($count!=0){
+    if($count!=-1){
         $id = rand(0,$count);
     $photo = $photos[$id];
-$image= '/autos/'.$photo;
+    $image= '/autos/'.$photo;
     }
     else{
         $image = '/images/5.jpg';
@@ -18,12 +18,12 @@ $image= '/autos/'.$photo;
 
 
     <div class="container text-center header-part">
-        <h2 class="header-text" style="font-size: 40px;"><span class="rotate">Una Familia Comprometida,Nuestro Compromiso no termina Aqui</span>
+        <h2 class="" style="font-size: 60px;"><span class="rotate">Una Familia Comprometida,Nuestro Compromiso no termina Aqui</span>
         </h2>
-        <h2 class="header-text"><small style="color: whitesmoke;"><strong>Pagamos al Contado</strong></small></h2>
+        <h2 class="h2 pull-right" style="font-size: 60px;"><small style="color: #f51200;"><strong>Pagamos al Contado</strong></small></h2>
 
         <div class="angle-down">
-            <a href="#Practice_Area">
+            <a href="#Practice_Area" style="color:#B5F1E3;padding-left: 370px;">
                 <i class="fa fa-angle-double-down fa-4x wow animated fadeInDown"
                    data-wow-iteration="infinite"></i>
             </a>
