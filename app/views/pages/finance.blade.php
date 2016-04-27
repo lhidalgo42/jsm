9ac407ce56bb8d5fdd3c5f6fac787ebc5faf8cc3
Leonardo Hidalgo <lhidalgo@alumnos.uai.cl>
2016-04-27 01:26:05 -0300

    SE ACHICA BOTON DE  financiamiento

--- app/views/pages/finance.blade.php
+++ app/views/pages/finance.blade.php
@@ -21,7 +21,7 @@
                 </div>
             </div>
             <div class="col-xs-12 col-md-12 ">
-                <button class="btn btn-block btn-lg btn-primary" id="btn-finance">Obtener Financiamiento</button>
+                <button class="btn btn-block btn-primary" id="btn-finance">Obtener Financiamiento</button>
             </div>
             <!-- Contact -->
 
