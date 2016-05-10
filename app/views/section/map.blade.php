6e55c4eda2b5a08f24d9f3ba51374a586c21a48f
Leonardo Hidalgo <lhidalgo@alumnos.uai.cl>
2016-05-10 14:12:32 -0300

    update map

--- app/views/section/map.blade.php
+++ app/views/section/map.blade.php
@@ -1,5 +1,5 @@
 <div class="wow animated zoomIn" data-wow-delay="1s" style="margin-bottom: -8px;">
     <iframe class="maps "
-            src="https://www.google.com/maps/embed/v1/place?q=place_id:ElBQZWRybyBBZ3VpcnJlIENlcmRhIDQ3MDAsIExvcyBDZXJyaWxsb3MsIENlcnJpbGxvcywgUmVnacOzbiBNZXRyb3BvbGl0YW5hLCBDaGlsZQ&key=AIzaSyB-Qcz4DBaKcLIrFEEkos4c5aWTpoNsme4"
+            src="https://www.google.com/maps/embed/v1/place?q=place_id:ElZDZXJyaWxsb3MgNDcwMCwgUGVkcm8gQWd1aXJyZSBDZXJkYSwgUGVkcm8gQWd1aXJyZSBDZXJkYSwgUmVnacOzbiBNZXRyb3BvbGl0YW5hLCBDaGlsZQ&key=AIzaSyCZWVU-X8K21FDxKJ7O9gAIIAXQmUsItZA"
             height="350" style="border:0"></iframe>
 </div>
